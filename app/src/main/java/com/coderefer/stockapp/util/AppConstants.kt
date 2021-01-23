c240eabfa66a652dd4abe2e9f4b6282e0b8ce3d6
Vamsi Tallapudi <vamsi@coderefer.com>
2021-01-23 16:00:26 +0530

    removing rapid api constant to avoid expose in github

--- app/src/main/java/com/coderefer/stockapp/util/AppConstants.kt
+++ app/src/main/java/com/coderefer/stockapp/util/AppConstants.kt
@@ -2,7 +2,6 @@ package com.coderefer.stockapp.util
 
 const val NETWORK_TIMEOUT = 15L
 const val YAHOO_API_ID_KEY = "x-rapidapi-key"
-const val YAHOO_API_ID = "af01a036a1msh807d37a550c52f6p12c4d4jsn057c2d3329e0"
 const val YAHOO_API_HOST_KEY = "x-rapidapi-host"
 const val YAHOO_API_HOST = "apidojo-yahoo-finance-v1.p.rapidapi.com"
 const val WEATHER_UNIT = "metric"
